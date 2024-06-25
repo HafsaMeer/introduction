d2cd5a09a3698312b6125a0ca19ab3919c911719
HafsaMeer <hafsabutt1997@gmail.com>
2024-06-25 17:06:27 +0500

    Update Firstclass.java

--- src/introduction/Firstclass.java
+++ src/introduction/Firstclass.java
@@ -5,7 +5,6 @@ public class Firstclass {
 	public static void main(String[] args) {
 		
 		
-	       System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
 
 		//Invoke the browser
 		WebDriver driver = new ChromeDriver();
