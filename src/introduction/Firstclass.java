30115a29628a967fb0a34d053d08404f60acc7fa
HafsaMeer <hafsabutt1997@gmail.com>
2024-06-25 17:07:37 +0500

    invoke browser

--- src/introduction/Firstclass.java
+++ src/introduction/Firstclass.java
@@ -5,6 +5,7 @@ public class Firstclass {
 	public static void main(String[] args) {
 		
 		
+	      // System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
 
 		//Invoke the browser
 		WebDriver driver = new ChromeDriver();
